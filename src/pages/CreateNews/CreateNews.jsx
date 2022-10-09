00bbf066a07d78aaa73da8269b207bfd3301d546
flexible <89507068+jopaleti@users.noreply.github.com>
2022-10-09 15:31:35 +0100

    create-news-form

--- src/pages/CreateNews/CreateNews.jsx
+++ src/pages/CreateNews/CreateNews.jsx
@@ -1,18 +1,56 @@
 import React from "react";
+import "./CreateNews.scss";
 
 const CreateNews = () => {
-  //   const handleSubmit = () => {
-  //     // let payload = {
-  //     //     title,
-  //     //     descritpition,
-  //     //     author,
-  //     //     dateOfNews,
-  //     //     image,
-  //     //     videoLink,
-  //     // category - should be a dropdown
-  //     // }
-  //   };
-  return <div>Create News</div>;
+	//   const handleSubmit = () => {
+	//     // let payload = {
+	//     //     title,
+	//     //     descritpition,
+	//     //     author,
+	//     //     dateOfNews,
+	//     //     image,
+	//     //     videoLink,
+	//     // category - should be a dropdown
+	//     // }
+	//   };
+	return (
+		<div>
+			<div className="blog-form">
+				<form>
+					<div className="title">
+						<label htmlFor="title">Title</label>
+						<input type="text" placeholder="Title" />
+					</div>
+					<div className="author">
+						<label htmlFor="title">Author</label>
+						<input type="text" placeholder="Author" />
+					</div>
+					<div className="videolink">
+						<label htmlFor="title">VideoLink</label>
+						<input type="text" placeholder="https://youtube.leyu12" />
+					</div>
+					<div className="image">
+						<label htmlFor="title">Upload Image</label>
+						<input
+							type="file"
+							id="image"
+							name="image"
+							accept="image/png, image/jpeg"
+						/>
+					</div>
+					<div className="desc">
+						<label htmlFor="desc">Description</label>
+						<textarea
+							name="desc"
+							id=""
+							rows="10"
+							placeholder="Description"
+						></textarea>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default CreateNews;
