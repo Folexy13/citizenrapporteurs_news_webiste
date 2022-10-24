310f3d5961822fdcf5598d9d4ed767b0acb13689
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-10-24 11:11:56 +0100

    ...

--- src/redux/action/newsAction.js
+++ src/redux/action/newsAction.js
@@ -4,7 +4,7 @@ import { userConstants } from "../../constant/userConstants";
 import axios from "../../helpers/api";
 import { trackPromise } from "react-promise-tracker";
 
-const BASE_API_URL = "https://citizen-rapporteur-api.herokuapp.com";
+const BASE_API_URL = "https://cr-news-api.herokuapp.com";
 export const newsAction = {
   postNews,
   postComment,
