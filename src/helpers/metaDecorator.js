99de1a2c39e65d30105783341462827771c96614
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-10-27 01:52:34 +0100

    ...

--- src/helpers/metaDecorator.js
+++ src/helpers/metaDecorator.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 import { truncateText } from "../components/card/Card";
 
 const MetaDecorator = ({ description, imageUrl, imageAlt }) => (
-  <Helmet>
+  <>
     <meta name="description" content={truncateText(description, 80)} />
     <meta property="og:description" content={truncateText(description, 80)} />
     <meta property="og:image" itemProp="image" content={imageUrl} />
@@ -12,7 +12,7 @@ const MetaDecorator = ({ description, imageUrl, imageAlt }) => (
     <meta property="og:image:width" content="300" />
     <meta property="og:url" content={window.location.href} />
     <meta property="og:image:height" content="300" />
-  </Helmet>
+  </>
 );
 
 MetaDecorator.propTypes = {
