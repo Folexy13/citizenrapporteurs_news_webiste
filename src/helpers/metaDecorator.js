6367828a9aec1aba64d11530af1f72d8f90a1473
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-10-27 01:15:07 +0100

    ...

--- src/helpers/metaDecorator.js
+++ src/helpers/metaDecorator.js
@@ -1,25 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
+import { truncateText } from "../components/card/Card";
 
-const MetaDecorator = ({ title, description, imageUrl, imageAlt }) => (
+const MetaDecorator = ({ description, imageUrl, imageAlt }) => (
   <Helmet>
-    <title>{title}</title>
     <meta property="og:title" content={title} />
-    <meta name="description" content={description} />
-    <meta property="og:description" content={description} />
-    <meta
-      property="og:image"
-      content={"https://citizenrapporteurs.com" + imageUrl}
-    />
-    <meta
-      property="og:url"
-      content={
-        "https://citizenrapporteurs.com" +
-        window.location.pathname +
-        window.location.search
-      }
-    />
+    <meta name="description" content={truncateText(description, 80)} />
+    <meta property="og:description" content={truncateText(description, 80)} />
+    <meta property="og:image" content={imageUrl} />
+    <meta property="og:image:secure_url" content={imageUrl} />
+
+    <meta property="og:url" content={window.location.href} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image:alt" content={imageAlt} />
     <meta name="twitter:site" content={"@citizenRapporteur"} />
