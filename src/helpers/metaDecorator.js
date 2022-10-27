eb18485f64a7ff16da7161439c1d47051f9ef076
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-10-27 02:57:51 +0100

    ...

--- src/helpers/metaDecorator.js
+++ src/helpers/metaDecorator.js
@@ -3,24 +3,11 @@ import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { truncateText } from "../components/card/Card";
 
-const MetaDecorator = ({ description, imageUrl, imageAlt }) => (
-  <Helmet
-    encodeSpecialCharacters={true}
-    meta={[
-      {
-        name: "description",
-        content: truncateText(description, 80),
-      },
-      {
-        property: "og:image",
-        itemProp: "image",
-        content: imageUrl,
-      },
-    ]}
-  >
-    {/* <meta name="description" content={truncateText(description, 80)} /> */}
+const MetaDecorator = ({ description, imageUrl }) => (
+  <Helmet>
+    <meta name="description" content={truncateText(description, 80)} />
     <meta property="og:description" content={truncateText(description, 80)} />
-    {/* <meta property="og:image" itemProp="image" content={imageUrl} /> */}
+    <meta property="og:image" itemProp="image" content={imageUrl} />
     <meta property="og:image:secure_url" content={imageUrl} />
     <meta property="og:image:width" content="300" />
     <meta property="og:image:height" content="300" />
