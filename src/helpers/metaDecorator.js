b4c4464128fdb43d4c76e366341788b2313f25ef
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-10-27 01:40:27 +0100

    ...

--- src/helpers/metaDecorator.js
+++ src/helpers/metaDecorator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
-import { truncateText } from "../components/card/Card";
+// import { truncateText } from "../components/card/Card";
 
 const MetaDecorator = ({ description, imageUrl }) => (
   <Helmet>
