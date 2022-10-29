2d7ccd240b0665ab09b01deee8b48cca80737e46
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-10-29 12:28:27 +0100

    ...

--- src/redux/action/newsAction.js
+++ src/redux/action/newsAction.js
@@ -78,8 +78,9 @@ function getLatestNews() {
   };
 }
 function getSingleNews(payload) {
+  const { id } = payload;
   return (dispatch) => {
-    axios.post(`${BASE_API_URL}/single-news/`, payload).then((res) => {
+    axios.post(`${BASE_API_URL}/single-news/`, id).then((res) => {
       dispatch({
         type: userConstants.GET_SINGLE_NEWS,
         news: res.data.news,
