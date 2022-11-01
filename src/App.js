6fde18a741bdcc58d622f060c977241ed94a6204
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-11-01 23:47:14 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import {
 } from "react-router-dom";
 import { Navbar, Sidebar } from "./components";
 import { useLayoutEffect, useState } from "react";
-// import { newsAction } from "./redux/action/newsAction";
+import { newsAction } from "./redux/action/newsAction";
 import { useDispatch } from "react-redux";
 const Wrapper = ({ children }) => {
   const location = useLocation();
@@ -34,9 +34,9 @@ function App() {
     setShow(!show);
   };
   useEffect(() => {
-    // setInterval(() => {
-    //   dispacth(newsAction.getClickedNews());
-    // }, 1000);
+    setInterval(() => {
+      dispacth(newsAction.getClickedNews());
+    }, 1000);
   }, [dispacth]);
 
   return (
