866ba8887c257c0b32defed97be5f4b8818abd19
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-12-03 08:41:04 +0100

    ...

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_API_URL = "https://cr-news-api.herokuapp.com";
-// const BASE_API_URL = "http://localhost:8080";
+// const BASE_API_URL = "https://cr-news-api.herokuapp.com";
+const BASE_API_URL = "http://localhost:8080";
 
 const AxiosInstance = axios.create({
   baseURL: BASE_API_URL,
