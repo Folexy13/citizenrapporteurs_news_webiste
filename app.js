ba47d63f23b4a31e0a5cb8ee02b2dfa4e0d83e5c
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-12-03 08:24:02 +0100

    ..

--- app.js
+++ app.js
@@ -15,31 +15,31 @@ app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", __dirname + "/views");
 // cors middleware
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "https://citizenrapporteurs.com/",
-    "https://citizenrapporteurs.com",
-    "https://www.citizenrapporteurs.com/",
-    "https://www.citizenrapporteurs.com",
-    "https://citizen-rapporteur.netlify.app/",
-    "https://citizen-rapporteur.netlify.app",
-    "http://localhost:3000",
-    "http://localhost:8080",
-    "https://www.pluralhealthcareltd.com/",
-    "https://www.pluralhealthcareltd.com",
-    "https://pluralhealthcareltd.com/",
-    "https://pluralhealthcareltd.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-  return next();
-});
-// app.use(cors("*"));
+// app.use((req, res, next) => {
+//   const allowedOrigins = [
+//     "https://citizenrapporteurs.com/",
+//     "https://citizenrapporteurs.com",
+//     "https://www.citizenrapporteurs.com/",
+//     "https://www.citizenrapporteurs.com",
+//     "https://citizen-rapporteur.netlify.app/",
+//     "https://citizen-rapporteur.netlify.app",
+//     "http://localhost:3000",
+//     "http://localhost:8080",
+//     "https://www.pluralhealthcareltd.com/",
+//     "https://www.pluralhealthcareltd.com",
+//     "https://pluralhealthcareltd.com/",
+//     "https://pluralhealthcareltd.com",
+//   ];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   return next();
+// });
+app.use(cors());
 app.use(morgan("combined"));
 
 app.use(express.json());
