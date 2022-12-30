e486401b69896cbba73fc209a38431e6d9a7e8af
Folexy13 <folajimiopeyemisax13@gmail.com>
2022-12-30 22:33:42 +0100

    ...

--- controllers/index.js
+++ controllers/index.js
@@ -374,13 +374,10 @@ async function getClickedNews(req, res) {
 }
 async function login(req, res) {
   const { username, password } = req.body;
-  const token = jwt.sign({ username, password }, process.env.JWT_TOKEN, {
+  const token = jwt.sign({ username, password }, "process.env.JWT_TOKEN", {
     expiresIn: "3h",
   });
-  if (
-    username !== process.env.ADMIN_USER ||
-    password !== process.env.ADMIN_PASSWORD
-  ) {
+  if (username !== "citizen-admin" || password !== "adminffegg") {
     return res.json({
       status: false,
       message: "Incorrect Login Details",
---
+++ index.js
@@ -0,0 +1,49 @@
+const http = require("http");
+
+require("dotenv").config();
+
+const { mongoConnect } = require("./config/db.config");
+mongoConnect();
+const port = process.env.PORT || 8087;
+// const port = 8081;
+
+// const server = http.createServer(app);
+
+// async function startServer() {
+//   await mongoConnect();
+
+//   // server.listen(port, () => {
+//   //   console.log(`Listening on port ${port}...`);
+//   // });
+// }
+
+// startServer();
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+
+const morgan = require("morgan");
+
+const router = require("./routes/");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(morgan("combined"));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(router);
+
+app.get("/", (req, res) => {
+  res.send("Hi Live ");
+});
+
+// app.listen("5000", () => console.log("Listening on port 5000"));
+
+module.exports = app;
