ec9b05d69b09ad99d57d0f5ce18ff1d48633e692
Folexy13 <folajimiopeyemisax13@gmail.com>
2023-04-20 22:47:00 +0100

    ...

--- src/redux/action/newsAction.js
+++ src/redux/action/newsAction.js
@@ -2,8 +2,8 @@
 import { alertActions } from "./alertAction";
 import { userConstants } from "../../constant/userConstants";
 import axios from "../../helpers/api";
-// export const BASE_API_URL = "https://cr_new_api.deta.dev";
-export const BASE_API_URL = "https://cnew-api.onrender.com";
+export const BASE_API_URL = "https://cr_new_api.deta.dev";
+// export const BASE_API_URL = "https://cnew-api.onrender.com";
 // export const BASE_API_URL = "http://localhost:5000";
 export const newsAction = {
   postNews,
