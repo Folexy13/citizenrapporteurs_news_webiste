21b1cea27b767013d6baaa6f90e58df9979f5fbb
Folajimi <folajimiopeyemisax13@gmail.com>
2023-07-27 02:28:02 +0100

    ..

--- src/redux/action/newsAction.js
+++ src/redux/action/newsAction.js
@@ -4,7 +4,7 @@ import { userConstants } from "../../constant/userConstants";
 import axios from "../../helpers/api";
 import { getIPAddress } from "../../helpers/getIP";
 //export const BASE_API_URL = "https://cr_new_api.deta.dev";
-export const BASE_API_URL = "https://attractive-housecoat-fox.cyclic.app";
+export const BASE_API_URL = "https://pcgseduporch.com/api/news/";
 // export const BASE_API_URL = "http://localhost:8087";
 const uuid = localStorage.getItem("uuid");
 
